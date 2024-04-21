3bd8cad2e46608f86aee6d4576dee355a3813c54
Erick Mairinque <contact.emairink@gmail.com>
2024-04-21 20:37:58 +0000

    Release Staging environment with 2 changes made by Erick Mairinque

--- sections/HelloWorld.tsx
+++ sections/HelloWorld.tsx
@@ -1,3 +1,16 @@
-export default function() {
-    return "Hello World"
+interface col {
+  value : String
+}
+
+export default function (props : col) {
+
+  return <>
+    {/* <span class="bg-amber-300 text-stone-950">Hello World </span> */}
+    {props.value}
+    <div class="flex justify-center">
+      <input class="w-10" type="color" id="html5colorpicker" value="#ff0000" />
+      <input class="w-10" type="color" id="html5colorpicker" value="#00ff00" />
+      <input class="w-10" type="color" id="html5colorpicker" value="#0000ff" />
+    </div>
+  </>
 }
